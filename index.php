08d4257c9d5c1a1ff873d0b0b28501a49547531c
Darshan Agravat <darshanr.agravat@gmail.com>
2025-01-05 21:09:59 +0530

    Initial Update 05_01_2024 #1

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>The Crimson Lotus Salon</title>
+
+    <?php
+        include 'inc/head.php';
+    ?>
+</head>
+<body>
+    <section id="header_section">
+        <?php
+            include 'inc/front/header.php';
+        ?>
+    </section>
+
+    <section id="main_section">
+
+    </section>
+
+    <section id="footer_section">
+        <?php
+            include 'inc/front/footer.php';
+        ?>
+    </section>
+</body>
+</html>
